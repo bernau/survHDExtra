993e0b51f60ad5ffab29a016fdd8bb67892273ff
bernau <bernau@bernau1.ibe.med.uni-muenchen.de>
2012-09-13 12:59:43 +0200

    changed vignette

--- R/AllClasses.r
+++ R/AllClasses.r
@@ -1,4 +1,4 @@
-###all classes ######------###
+###all classes ######------###---###===
 
 setClass(Class = "gsagenesets", representation(genesets = "list", geneset.names = "character", 
 				geneset.descriptions = "character", geneset.direction = "character"))
